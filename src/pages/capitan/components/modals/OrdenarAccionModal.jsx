6dc797c2b8c7b5c880025edcc2a397f5b1f98383
Pablo Leyes <64818001+Yaguaret3@users.noreply.github.com>
2024-05-29 01:36:28 -0300

    Capitan UI: Sockets

--- src/pages/capitan/components/modals/OrdenarAccionModal.jsx
+++ src/pages/capitan/components/modals/OrdenarAccionModal.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, {useContext, useState} from 'react'
 import {Modal, Grid, Box, Button, Typography, Autocomplete, TextField} from '@mui/material'
 import service from '../../Service'
 import ActionCard from "../../../common/ActionCard.jsx";
+import {CapitanContext} from "../../Context.jsx";
+import useWebSocket from "../../../../hooks/useWebSocket.jsx";
 
 const OrdenarAccionModal = ({ open, handleClose, cards, subregions }) => {
+
+    const {stompClient} = useContext(CapitanContext);
+    const {disparoCapitanes, disparoControl} = useWebSocket({});
 
     const [cardSelected, setCardSelected] = useState(undefined);
     const [subregionSelected, setSubregionSelected] = useState({});
@@ -29,17 +34,20 @@ const OrdenarAccionModal = ({ open, handleClose, cards, subregions }) => {
         setLabelSubregionSelected(newValue);
     }
 
-    const handleService = () => {
+    const handleService = async () => {
 
         if(cardSelected === undefined){
             alert('Por favor, seleccione una carta de acción válida');
             return;
         }
 
-        service.playActionCard({
+        await service.playActionCard({
             subregionId: subregionSelected.id,
             cardId: cardSelected.id
         })
+        disparoControl({stompClient:stompClient});
+        disparoCapitanes({stompClient:stompClient});
+        handleClose();
     }
 
     return (
