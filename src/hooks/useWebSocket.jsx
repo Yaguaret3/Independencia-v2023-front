2d1f9a5eee2e3171834672e2f6106d7887e79aad
Pablo Leyes <n.leyes.n@gmail.com>
2025-06-21 04:35:47 -0300

    General:WebSockets connections limited.

--- src/hooks/useWebSocket.jsx
+++ src/hooks/useWebSocket.jsx
@@ -1,16 +1,41 @@
+import {Bounce, toast} from "react-toastify";
+
 const useWebSocket = ({channel, fetchData}) => {
 
 
     const conectarWS = ({stompClient}) => {
 
-        stompClient.connect({}, function (frame) {
-            stompClient.subscribe(channel, function (valorFinal) {
-                fetchData();
-            });
-            stompClient.subscribe('/actualizar-todos', function (valorFinal) {
-                fetchData();
-            });
-        });
+        stompClient.connect(
+            {
+                Authorization: 'Bearer ' + localStorage.getItem('independencia-token')
+            },
+            function (frame) {
+                stompClient.subscribe(channel, function (valorFinal) {
+                    fetchData();
+                });
+                stompClient.subscribe('/actualizar-todos', function (valorFinal) {
+                    fetchData();
+                });
+            },
+            function (error) {
+
+                const longError = error.headers.message;
+                const errorSplit = longError.split('\\c');
+                const errorToShow = errorSplit[1];
+
+                toast.error(errorToShow, {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                    transition: Bounce,
+                });
+            }
+        );
     }
 
     const disparoControl = ({stompClient}) =>{
