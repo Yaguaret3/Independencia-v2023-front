2491d422d2aa967eb86663ed08525c3e16c0edbd
Pablo Leyes <n.leyes.n@gmail.com>
2025-08-29 03:37:53 -0300

    Settings: Remove Role

--- src/pages/settings/components/Users.jsx
+++ src/pages/settings/components/Users.jsx
@@ -1,8 +1,8 @@
 import React, {useContext, useState} from 'react';
 import {Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 import {SettingsContext} from "../Context.jsx";
-import useWebSocket from "../../../hooks/useWebSocket.jsx";
 import AddRoleModal from "./modals/AddRoleModal.jsx";
+import service from "../Service.js";
 
 const Users = () => {
 
@@ -18,6 +18,12 @@ const Users = () => {
     }
     const [playerSelected, setPlayerSelected] = useState({});
 
+    const handleRemoveRole = async ({u}) => {
+        if(confirm('¿Estás seguro de que deseás eliminar el rol ' + u.rol + ' de ' + u.username + '?')){
+            await service.removeRole({id: u.id, rol: u.rol});
+        }
+    }
+
     return (
         <>
             <TableContainer component={Paper}>
@@ -31,18 +37,28 @@ const Users = () => {
                                 Email
                             </TableCell>
                             <TableCell padding='none' align="center">
+                                Rol
+                            </TableCell>
+                            <TableCell padding='none' align="center">
                             </TableCell>
                         </TableRow>
 
                     </TableHead>
                     <TableBody>
                         {users?.map(u =>
-                            <TableRow>
+                            <TableRow key={u.id}>
                                 <TableCell padding='none' align="center">
                                     {u.username}
                                 </TableCell>
                                 <TableCell padding='none' align="center">
                                     {u.email}
+                                </TableCell>
+                                <TableCell padding='none' align="center">
+                                    <Button onClick={() => handleRemoveRole({u: u})}
+                                            size="small" variant='outlined'
+                                            color={'error'}>
+                                        {u.city ? u.rol + ' de ' + u.city : u.rol}
+                                    </Button>
                                 </TableCell>
                                 <TableCell padding='none' align="center">
                                     <Button onClick={() => handleOpenAddRole({u: u})}
