2bdc27bbc1e1ef7e504db3e709ffddb39baf7453
Pablo Leyes <n.leyes.n@gmail.com>
2025-09-16 18:57:41 -0300

    Caudillo: Fix websocket at Move Camp

--- src/pages/capitan/components/modals/CambiarRegionModal.jsx
+++ src/pages/capitan/components/modals/CambiarRegionModal.jsx
@@ -1,9 +1,14 @@
-import React, {useState} from 'react'
+import React, {useContext, useState} from 'react'
 import {Modal, Grid, Box, Button, Typography, Autocomplete, TextField} from '@mui/material'
 import service from '../../Service'
 import ActionCard from "../../../common/ActionCard.jsx";
+import {CapitanContext} from "../../Context.jsx";
+import useWebSocket from "../../../../hooks/useWebSocket.jsx";
 
 const CambiarRegionModal = ({open, handleClose, cards, regions}) => {
+
+    const {stompClient} = useContext(CapitanContext);
+    const {disparoCapitanes, disparoControl} = useWebSocket({});
 
     const [cardSelected, setCardSelected] = useState({});
     const [regionSelected, setRegionSelected] = useState({});
@@ -29,17 +34,21 @@ const CambiarRegionModal = ({open, handleClose, cards, regions}) => {
         setLabelRegionSelected(label);
     }
 
-    const handleService = () => {
+    const handleService = async () => {
 
         if (cardSelected === undefined) {
             alert('Por favor, seleccione una carta de acción válida');
             return;
         }
 
-        service.moverCampamento({
+        await service.moverCampamento({
             regionToId: regionSelected.id,
             cardId: cardSelected.id
         })
+
+        disparoControl({stompClient:stompClient});
+        disparoCapitanes({stompClient:stompClient});
+        handleClose();
     }
 
     return (
